fb9683a747f619f7cc8de001eea55bb1f1ca82c4
Yura <yura@yura.net>
2025-09-27 15:47:13 +0100

    works standalone

--- client/src/main/java/net/yura/shithead/client/ShitHeadClient.java
+++ client/src/main/java/net/yura/shithead/client/ShitHeadClient.java
@@ -1,4 +1,41 @@
 package net.yura.shithead.client;
 
-public class ShitHeadClient {
+import net.yura.mobile.gui.ActionListener;
+import net.yura.mobile.gui.Application;
+import net.yura.mobile.gui.DesktopPane;
+import net.yura.mobile.gui.KeyEvent;
+import net.yura.mobile.gui.components.Button;
+import net.yura.mobile.gui.components.Frame;
+import net.yura.mobile.gui.components.Label;
+import net.yura.mobile.gui.components.Panel;
+import javax.microedition.lcdui.Graphics;
+
+public class ShitHeadClient extends Application {
+
+    protected void initialize(DesktopPane dp) {
+
+        dp.setLookAndFeel(DesktopPane.getSystemLookAndFeelClassName());
+
+        Frame frame = new Frame("SwingME app");
+
+        frame.getContentPane().add(new Label("Hello ShitHead"));
+
+        Button exit = new Button("Exit");
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(String arg0) {
+                Application.exit();
+            }
+        });
+        exit.setMnemonic(KeyEvent.KEY_END); // press Esc to exit
+
+        Panel panel = new Panel();
+        panel.add(exit);
+        frame.getContentPane().add(panel, Graphics.BOTTOM);
+
+        //frame.pack(); // shrink to the smallest size
+        //frame.setLocationRelativeTo(null); // center
+
+        frame.setMaximum(true);
+        frame.setVisible(true);
+    }
 }
